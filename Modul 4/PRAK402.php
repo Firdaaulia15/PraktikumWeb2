3aaaa7045c062947a305336bcea7b773ecde4b1f
Firdaaulia15 <101546341+Firdaaulia15@users.noreply.github.com>
2022-05-01 15:21:54 +0800

    Add files via upload

---
+++ Modul 4/PRAK402.php
@@ -0,0 +1,73 @@
+<?php
+    $nilai = [
+        ["nama" => "Andi", "nim" => "2101001", "uts" => 87, "uas" => 65],
+        ["nama" => "Budi", "nim" => "2101002", "uts" => 76, "uas" => 79],
+        ["nama" => "Tono", "nim" => "2101003", "uts" => 50, "uas" => 41],
+        ["nama" => "Jessica", "nim" => "2101004", "uts" => 60, "uas" => 75],
+    ];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <style>
+        table, td, th {
+            border: 1px solid;
+            padding: 10px;
+        }
+        table {
+        border-collapse: collapse;
+        }
+        .bgcolor{
+            background-color: #BCBABA;
+        }
+        .bold{
+            font-weight: bold;
+        }
+        
+    </style>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PRAK402</title>
+</head>
+<body>
+    <table>
+        <tr class="bgcolor && bold">
+            <td>Nama</td>
+            <td>NIM</td>
+            <td>Nilai UTS</td>
+            <td>Nilai UAS</td>
+            <td>Nilai Akhir</td>
+            <td>Huruf</td>
+        </tr>
+        <?php
+        for ($i=0; $i < count($nilai); $i++) { 
+            $nilai[$i]["nilaiAkhir"] = $nilai[$i]["uts"] * 0.4 + $nilai[$i]["uas"] * 0.6;
+            if($nilai[$i]["nilaiAkhir"] >= 80){
+                $nilai[$i]["huruf"] = "A";
+            }elseif($nilai[$i]["nilaiAkhir"] > 70){
+                $nilai[$i]["huruf"] = "B";
+            }elseif($nilai[$i]["nilaiAkhir"] > 60){
+                $nilai[$i]["huruf"] = "C";
+            }elseif($nilai[$i]["nilaiAkhir"] > 50){
+                $nilai[$i]["huruf"] = "D";
+            }elseif($nilai[$i]["nilaiAkhir"] < 50){
+                $nilai[$i]["huruf"] = "E";
+            }
+        }
+
+        for ($i=0; $i < count($nilai); $i++) { 
+            echo "<tr>";
+            echo "<td padding = 10px>".$nilai[$i]["nama"]."</td>";
+            echo "<td>".$nilai[$i]["nim"]."</td>";
+            echo "<td>".$nilai[$i]["uts"]."</td>";
+            echo "<td>".$nilai[$i]["uas"]."</td>";
+            echo "<td>".$nilai[$i]["nilaiAkhir"]."</td>";
+            echo "<td>".$nilai[$i]["huruf"]."</td>";
+            echo "</tr>";
+        }       
+        ?>
+    </table>
+</body>
+</html>
---
+++ Modul 4/PRAK403.php
@@ -0,0 +1,106 @@
+<?php
+    $nilai = [
+        ["no" => 1, "nama" => "Ridho", 
+        "mk" => [
+            ["mataKuliah" =>"Pemrograman I", "sks" => 2], 
+            ["mataKuliah" => "Praktikum Pemrograman I", "sks" => 1],
+            ["mataKuliah" => "Pengantar Lingkungan Lahan Basah", "sks" => 2], 
+            ["mataKuliah" => "Arsitektur Komputer", "sks" => 3]
+        ]
+        ],
+
+        ["no" => 2, "nama" => "Ratna", 
+        "mk" => [
+            ["mataKuliah" =>"Basis Data I", "sks" => 2], 
+            ["mataKuliah" => "Praktikum Basis Data I", "sks" => 1],
+            ["mataKuliah" => "Kalkulus", "sks" => 3]
+        ]
+        ],
+
+        ["no" => 3, "nama" => "Tono", 
+        "mk" => [
+            ["mataKuliah" => "Rekayasa Perangkat Lunak", "sks" => 3], 
+            ["mataKuliah" => "Analisis dan Perancangan Sistem", "sks" => 3],
+            ["mataKuliah" => "Komputasi Awan", "sks" => 3], 
+            ["mataKuliah" => "Kecerdasan Bisnis", "sks" => 3]
+        ]
+        ]
+    ];
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+         table, td, th {
+            border: 1px solid;
+            padding: 10px;
+        }
+        table {
+        border-collapse: collapse;
+        }
+        tr, td, th {
+            padding: 8px;
+        }
+        .revisi{
+            background-color: #ff3333;
+        }
+        .Trevisi{
+            background-color: #009900;
+        }
+        .bgcolor{
+            background-color: #BCBABA;
+        }
+        .bold{
+            font-weight: bold;
+        }
+    </style>
+    <title>Soal 3</title>
+</head>
+<body>
+    <table>
+        <tr class="bgcolor && bold">
+            <td>No</td>
+            <td>Nama</td>
+            <td>Mata Kuliah diambil</td>
+            <td>SKS</td>
+            <td>Total SKS</td>
+            <td>Keterangan</td>
+        </tr>
+        <?php
+            for ($i=0; $i < count($nilai); $i++) {
+                $jumlah = 0;
+                for ($j=0; $j < count($nilai[$i]["mk"]); $j++) { 
+                    $jumlah += $nilai[$i]["mk"][$j]["sks"];
+                }
+                $nilai[$i]["jumlah"] = $jumlah;
+                if ($nilai[$i]["jumlah"] <= 6) {
+                    $nilai[$i]["keterangan"] = "<td class='revisi'>Revisi KRS</td>";
+                } else {
+                    $nilai[$i]["keterangan"] = "<td class='Trevisi'>Tidak Revisi</td>";
+                }
+            }
+            for ($i=0; $i < count($nilai); $i++) {
+                for ($j=0; $j < count($nilai[$i]["mk"]); $j++) { 
+                    echo "<tr>";
+                    if ($j == 0) {
+                        echo "<td>".$nilai[$i]["no"]."</td>";
+                        echo "<td>".$nilai[$i]["nama"]."</td>";
+                        echo "<td>".$nilai[$i]["mk"][$j]["mataKuliah"]."</td>";
+                        echo "<td>".$nilai[$i]["mk"][$j]["sks"]."</td>";
+                        echo "<td>".$nilai[$i]["jumlah"]."</td>";
+                        echo $nilai[$i]["keterangan"];
+                    }else {
+                        echo "<td></td>";
+                        echo "<td></td>";
+                        echo "<td>".$nilai[$i]["mk"][$j]["mataKuliah"]."</td>";
+                        echo "<td>".$nilai[$i]["mk"][$j]["sks"]."</td>";
+                        echo "<td></td>";
+                        echo "<td></td>";
+                    }
+                    echo "</tr>";
+                }
+            }
+        ?>
+    </table>
+</body>
+</html>
